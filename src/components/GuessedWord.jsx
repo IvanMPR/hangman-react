b5dd75717c979248768998cc02aab41d2f7413d5
IvanMPR <66476925+IvanMPR@users.noreply.github.com>
2026-09-26 17:25:20 +0200

    Add processWord functionality

--- src/components/GuessedWord.jsx
+++ src/components/GuessedWord.jsx
@@ -1,22 +1,27 @@
 import "./GuessedWord.css";
 
-export default function GuessedWord({ currentState, isTheGamePlayed }) {
-  // const processWord = (word, guessedLetter) => {
-  //   word
-  //     .trim()
-  //     .toUpperCase()
-  //     .split("")
-  //     .map(currentLetter => {
-  //       if (currentLetter === " ") return currentLetter;
-  //       if (currentLetter === guessedLetter.toUpperCase()) return currentLetter;
-  //       else return '_'
-  //     });
-  //     const parsedWord = processWord(currentState, guessedLetter)
-  // };
+export default function GuessedWord({
+  currentState,
+  isTheGamePlayed,
+  previousGuesses,
+}) {
+  function processWord(word) {
+    if (word) {
+      console.log(word, previousGuesses, "from processWord");
+      const parsedWord = word.split("").map(letter => {
+        if (letter === " ") return letter;
+        if (previousGuesses.includes(letter)) {
+          return letter;
+        } else return "_";
+      });
+      return parsedWord.join("");
+    }
+  }
+  const parsedWord = processWord(currentState);
   return (
     <div className='guessed-word-div'>
       <h2 className='guessed-word'>
-        {!isTheGamePlayed ? "guess the hidden word" : currentState}
+        {!isTheGamePlayed ? "guess the hidden word" : parsedWord}
       </h2>
     </div>
   );
