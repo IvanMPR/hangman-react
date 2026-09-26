fd7da8be6e6158a6172301b1b811db9e1d3f150e
IvanMPR <66476925+IvanMPR@users.noreply.github.com>
2026-09-26 11:13:19 +0200

    PreviousGuesses render logic set

--- src/components/PreviousGuesses.jsx
+++ src/components/PreviousGuesses.jsx
@@ -1,39 +1,20 @@
 import "./PreviousGuesses.css";
 
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 export default function PreviousGuesses() {
   return (
     <div className='previous-guesses-div'>
       <p className='previous-guesses-title'>Previous Guesses</p>
-
       <p className='previous-guesses'>
-        <span>A</span>
-        <span>B</span>
-        <span>C</span>
-        <span>D</span>
-        <span>E</span>
-        <span>F</span>
-        <span>G</span>
-        <span>H</span>
-        <br />
-        <span>I</span>
-        <span>J</span>
-        <span>K</span>
-        <span>L</span>
-        <span>M</span>
-        <span>N</span>
-        <span>O</span>
-        <span>P</span>
-        <span>Q</span>
-        <span>R</span>
-        <br />
-        <span>S</span>
-        <span>T</span>
-        <span>U</span>
-        <span>V</span>
-        <span>W</span>
-        <span>X</span>
-        <span>Y</span>
-        <span>Z</span>
+        {ALPHABET.split("").map(letter => {
+          return (
+            <span key={letter}>
+              {letter}
+              {(letter === "H" || letter === "R") && <br />}
+            </span>
+          );
+        })}
       </p>
     </div>
   );
